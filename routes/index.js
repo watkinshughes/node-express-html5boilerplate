ed2bc2560e85004acd60f8cef46cac736abfd083
Ryan WH <ryan@propercursive.com>
2011-10-26 06:51:35 -0500

    Initialize

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+/*
+ * GET portfolio page.
+ */
+
+exports.portfolio = function(req, res){
+  res.render('index', { title: 'Express' })
+};
